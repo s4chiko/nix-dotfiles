9b63e81d81b86137efd236e9c2a4bc3b8ed86df8
s4chiko <user@MBP.local>
2026-05-03 13:34:14 +0900

    Add Helix Setting

--- modules/helix.nix
+++ modules/helix.nix
@@ -12,5 +12,34 @@
         mouse = false;
       };
     };
+    languages = {
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter = {
+            command = "alejandra";
+            args = ["-q"];
+          };
+        }
+        {
+          name = "rust";
+          auto-format = true;
+        }
+      ];
+
+      language-server = {
+        rust-analyzer = {
+          config = {
+            checkOnSave = {
+              command = "clippy";
+            };
+            files = {
+              excludeDirs = [".direnv" ".git" "target"];
+            };
+          };
+        };
+      };
+    };
   };
 }
